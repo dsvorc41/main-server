a9349ed398d11ab341abc0df8ffe49610eefd121
dsvorc41 <danilosvorcan@live.com>
2017-02-26 16:09:42 -0800

    Adding mysql key to booksehlf.js

--- scripts/bookshelf.js
+++ scripts/bookshelf.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
   connection: {
     host: 'localhost',
     user: 'root',
-    password: '',  // set a password if needed
+    password: 'MyNewPass',  // set a password if needed
     database: 'thesis',  // Be sure to create DB on server
     charset: 'utf8'
   }
